bb1e107dfdd6efbc71a53a13ae88d50f378cbcdc
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-06-19 20:03:47 +0300

    final commit

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -62,7 +62,7 @@ export default function Header() {
         </tr>
       </table>
 
-      <h2>View the Visuals in this  Section</h2>
+      <h2>View the Dashboard in this  Section</h2>
       <table>
         <tr>
           <th>Page Routes </th>
