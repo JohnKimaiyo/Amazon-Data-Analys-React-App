693eedb02b2ca64e1ee14ef0d9b24e6cf0db9eeb
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-10-27 17:15:06 +0300

    final commit

---
+++ Backend/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+const app = express();
+import { Users } from "./amazon.js";
+import cors from "cors";
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  const { q } = req.query;
+
+  const keys = ["Uniq Id", "Product Name", "Category" , "Selling Price", "Model Number" , "About Product"   , "Product Specification"," Technical Details" ," Shipping Weight"  ,   " Image ",  "Product Url",  "Is Amazon Seller"  ];
+
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(q))
+    );
+  };
+
+  q ? res.json(search(Users).slice(0, 10)) : res.json(Users.slice(0, 10));
+});
+
+app.listen(5000, () => console.log("API is working!"));  
